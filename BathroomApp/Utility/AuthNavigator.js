f91a96a29e91714dcd89fd737db3b9d07819c656
Brist <vc5135ij@vc5135ij-g8b71.workstations.winona.edu>
2019-03-30 16:42:57 -0500

    removed authloading

--- BathroomApp/Utility/AuthNavigator.js
+++ BathroomApp/Utility/AuthNavigator.js
@@ -3,12 +3,10 @@ import React from 'react';
 import { createStackNavigator } from 'react-navigation';
 import LoginScreen from '../Pages/LoginScreen';
 import SignupScreen from '../Pages/SignupScreen';
-import AuthLoadingScreen from '../Pages/AuthLoadingScreen';
 
 const AuthNav = createStackNavigator({
    LoginScreen: LoginScreen,
-   SignupScreen: SignupScreen,
-   AuthLoadingScreen: AuthLoadingScreen
+   SignupScreen: SignupScreen
   },
   {
     initialRouteName: "LoginScreen"
