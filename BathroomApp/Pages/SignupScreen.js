52717078ae49e02fe81b8d816021d9e3b8377d14
Brist <vc5135ij@vc5135ij-g8b71.workstations.winona.edu>
2019-03-30 16:10:57 -0500

    added account creation

--- BathroomApp/Pages/SignupScreen.js
+++ BathroomApp/Pages/SignupScreen.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Platform, StyleSheet, Text, View, Button, TextInput, TouchableHighlight, Image, Alert } from 'react-native';
+import * as firebase from 'firebase';
 
 
 export default class LoginScreen extends Component {
@@ -23,9 +24,8 @@ export default class LoginScreen extends Component {
           <Image style={styles.inputIcon} source={{uri: 'https://img.icons8.com/ultraviolet/40/000000/gender-neutral-user.png'}}/>
           <TextInput autoCapitalize="none" style={styles.inputs}
               placeholder="Username"
-              secureTextEntry={true}
               underlineColorAndroid='transparent'
-              onChangeText={(password) => this.setState({username})}/>
+              onChangeText={(username) => this.setState({username})}/>
         </View>
 
 
@@ -47,11 +47,23 @@ export default class LoginScreen extends Component {
               onChangeText={(password) => this.setState({password})}/>
         </View>
 
-        <TouchableHighlight style={[styles.buttonContainer, styles.loginButton]} onPress={() => this.onClickListener('create')}>
+        <TouchableHighlight style={[styles.buttonContainer, styles.loginButton]} onPress={this._handleCreatePressed}>
           <Text style={styles.loginText}>Create Account</Text>
         </TouchableHighlight>
       </View>
     );
+  }
+
+  _handleCreatePressed = () => {
+    firebase.auth().createUserWithEmailAndPassword(this.state.email, this.state.password)
+    .then(() => {
+      //Push the UID and username to the SQL server
+      //this.state.username
+      //firebase.auth().currentUser.UID i think would give UID, needs to be tested
+      this.props.navigation.navigate('AppNav');
+    }).catch((error) => {
+      alert(error.message);
+    });
   }
 }
 
