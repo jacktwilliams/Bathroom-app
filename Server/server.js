1d018a6e32761965af78bd65b10ad04cd4165802
Jack Williams <jwilliams17@winona.edu>
2019-03-30 13:31:09 -0500

    Server now takes an institution and returns a json object with buildings and bathrooms and other institution data.

--- Server/server.js
+++ Server/server.js
@@ -13,37 +13,52 @@ var conn = mysql.createConnection({
 
 conn.connect();
 
+
 app.get('/', (req, res) => {
+  let errorHandler = function(error, extraMessage) {
+    console.log(error);
+    let additional = extraMessage ? extraMessage : "";
+    res.status(400).send('Error querying DB\n' + additional);
+  }
+
   console.log('Test. Return WSU bathrooms for Watkins');
-  let queryString = "select * from bathroom "; 
 
   let queryInstitution = req.query.institution;
-  let queryBuilding = req.query.building;
-  let queryGender = req.query.gender;
-
-  if(queryGender) {
-    queryString += "where bathroom.gender = '" + queryGender + "'";
-  }
-
-  if(queryInstitution && queryBuilding) {
-    if(queryGender) {
-      
-    }
-    queryString += "where bathroom.build_id in " +
-    "(select build_id from building where build_name = '" + queryBuilding + "' and org_id in " +
-    "(select org_id from organization where org_name = '" + queryInstitution + "'));"
-  }
-  else if (queryInstitution) {
-    queryString += "where bathroom.org_id in " +
-      "(select org_id from organization where org_name = '" + queryInstitution + "');";
-  }
+  let queryString = "select * from organization where org_name = '" + queryInstitution + "'";
+  let institutionFullInfo;
+  
   conn.query(queryString, (error, results) => {
     if ( error ){
-      console.log(error);
-      res.status(400).send('Error in database operation');
+      errorHandler(error);
     } else {
       console.log(results);
-      res.send(results);
+      //got city info. Get bathrooms
+      institutionFullInfo = results[0];
+
+      queryString = "select * from building where org_id = " + results[0].org_id + ";"
+      conn.query(queryString, (error1, results1) => {
+        if (error1) {
+          errorHandler(error1);
+        }
+        else {
+          console.log(results1);
+          institutionFullInfo.buildings = results1;
+          console.log("Full info\n" + JSON.stringify(institutionFullInfo));
+
+          queryString = "select * from bathroom where org_id = " + results[0].org_id +
+            " and build_id in (select build_id from organization where org_id = " + results[0].org_id + ");";
+          conn.query(queryString, (error2, results2) => {
+            if(error2) {
+              errorHandler(error2);
+            }
+            else {
+              console.log(results2);
+              institutionFullInfo.allBathrooms = results2;
+              res.status(200).send(institutionFullInfo);
+            }
+          });
+        }
+      });
     }
   });
 });
