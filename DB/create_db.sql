4291ff0fbd742ba7977c3b72c5e02e762fe399f1
Jack Williams <jwilliams17@winona.edu>
2019-04-05 17:33:23 -0500

    Adding database changes.

--- DB/create_db.sql
+++ DB/create_db.sql
@@ -1,25 +1,31 @@
+DROP DATABASE bathroom;
 CREATE DATABASE bathroom;
 USE bathroom;
 
 CREATE TABLE user (
-	user_id SERIAL,
-    username VARCHAR(25),
+	user_id BIGINT UNSIGNED NOT NULL,
+    user_name VARCHAR(64),
+    user_email VARCHAR(64),
     PRIMARY KEY(user_id)
 );
 
-CREATE TABLE organization (
-	org_id SERIAL NOT NULL,
-    org_name VARCHAR(75) NOT NULL,
-    latitude DECIMAL(9,6),
-    longitude DECIMAL(9,6),
-    city_name VARCHAR(50) NOT NULL,
+CREATE TABLE organization(
+    org_id SERIAL NOT NULL,
+    org_name VARCHAR(64) NOT NULL,
+    org_city VARCHAR(64) NOT NULL,
+    org_state VARCHAR(64) NOT NULL,
+    org_country VARCHAR(64) NOT NULL,
+    latitude DECIMAL(10,7),
+    longitude DECIMAL(10,7),
     PRIMARY KEY (org_id)
 );
 
 CREATE TABLE building (
-	build_id TINYINT UNSIGNED AUTO_INCREMENT NOT NULL,
     org_id BIGINT UNSIGNED NOT NULL,
-    build_name varchar(50) NOT NULL,
+	build_id TINYINT UNSIGNED AUTO_INCREMENT NOT NULL,
+    build_name varchar(64) NOT NULL,
+    latitude DECIMAL(10,7),
+    longitude DECIMAL(10,7),
     PRIMARY KEY(build_id, org_id),
     FOREIGN KEY (org_id)
 		REFERENCES organization(org_id)
@@ -28,11 +34,16 @@ CREATE TABLE building (
 );
 
 CREATE TABLE bathroom (
-	bath_id TINYINT UNSIGNED NOT NULL AUTO_INCREMENT UNIQUE,
     org_id BIGINT UNSIGNED NOT NULL,
     build_id TINYINT UNSIGNED NOT NULL,
-    bath_name VARCHAR(50) NOT NULL,
-    gender CHAR(1) NOT NULL, #M F O male female other
+	bath_id TINYINT UNSIGNED NOT NULL AUTO_INCREMENT UNIQUE,
+    /* bath_name VARCHAR(64) NOT NULL, */
+    room_num SMALLINT,
+    floor_num TINYINT NOT NULL,
+    loc_desc TEXT NOT NULL,
+    change_station BIT NOT NULL, /* 0 false, 1 true */
+    multi_person BIT NOT NULL, /* 0 false, 1 true */
+    gender VARCHAR(1) NOT NULL, /* #M F O male female other */
     PRIMARY KEY(org_id, build_id, bath_id),
 	FOREIGN KEY (org_id)
 		REFERENCES organization(org_id)
@@ -51,9 +62,12 @@ CREATE TABLE review (
     bath_id TINYINT UNSIGNED NOT NULL,
     user_id BIGINT UNSIGNED NOT NULL,
     review_text TEXT,
-    stars DECIMAL(2, 1) NOT NULL, #0-9 with tenth values. Must constrain by other means to 0-5 with .5 increments
+    stars DECIMAL(2, 1) NOT NULL, /* #0-9 with tenth values. Must constrain by other means to 0-5 with .5 increments */
+    wifi_stars DECIMAL(2, 1),
+    cell_stars DECIMAL(2, 1),
+    cell_providor VARCHAR(32),
     flags SET('gross', 'biohazard'),
-    date_time DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP, #auto set to submission time. Updated on edit.
+    date_time DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP, /* #auto set to submission time. Updated on edit. */
     PRIMARY KEY(review_id),
     FOREIGN KEY (org_id)
 		REFERENCES organization(org_id)
